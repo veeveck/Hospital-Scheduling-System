3c6944c4e2874d91d705a717f4446af2a53f907b
Vivek <veeveckjeet@gmail.com>
2023-07-20 00:01:49 -0400

    Register n Login Form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/RegisterStyle.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="form-container">
+      <Form
+        layout="vertical"
+        onFinish={onFinishHandler}
+        className="register-form"
+      >
+        <h3 className="text-center">Login</h3>
+        <Form.Item label="Email" name="email">
+          <Input type="email" placeholder="Enter Email" required />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" placeholder="Enter Password" required />
+        </Form.Item>
+        <Link to="/register" className="m-2">
+          Not A User , Register!!
+        </Link>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </Form>
     </div>
   );
 };
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/RegisterStyle.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Register</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" placeholder="Enter Name" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" placeholder="Enter Email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" placeholder="Enter Password" required />
+          </Form.Item>
+          <Link to="/login" className="m-2">
+            Already Registered, Login!!
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </Form>
+      </div>
+    </>
   );
 };
 
