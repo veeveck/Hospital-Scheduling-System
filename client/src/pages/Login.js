d6000c0f1df91c198d5e81956d806ccbcde6e459
Vivek <veeveckjeet@gmail.com>
2023-07-21 17:46:19 -0400

    React-Redux toolkit setup and spinner component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,12 +3,17 @@ import { Form, Input, message } from "antd";
 import "../styles/RegisterStyle.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const onFinishHandler = async (values) => {
     try {
+      dispatch(showLoading());
       const res = await axios.post("/api/v1/user/login", values);
+      dispatch(hideLoading());
       if (res.data.success) {
         localStorage.setItem("token", res.data.token);
         message.success("Login Succesfully");
@@ -17,6 +22,7 @@ const Login = () => {
         message.error(res.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       console.log(error);
       message.error("Something went wrong");
     }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,13 +3,17 @@ import { Form, Input, message } from "antd";
 import "../styles/RegisterStyle.css";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 
 const Register = () => {
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const onFinishHandler = async (values) => {
     try {
+      dispatch(showLoading());
       const res = await axios.post("/api/v1/user/register", values);
+      dispatch(hideLoading());
       if (res.data.success) {
         message.success("Registered Successfully");
         navigate("/login");
@@ -17,6 +21,7 @@ const Register = () => {
         message.error(res.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       console.log(error);
       message.error("Something went wrong");
     }
