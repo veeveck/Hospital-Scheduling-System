8555e719eeba042bf2af12c5046cc4348fafbc2e
Vivek <veeveckjeet@gmail.com>
2023-07-21 22:09:35 -0400

    Layout Setup

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import Layout from "../components/Layout";
 
 const HomePage = () => {
   //User data
@@ -23,7 +24,9 @@ const HomePage = () => {
   }, []);
   return (
     <div>
-      <h1>HomePage</h1>
+      <Layout>
+        <h1>HomePage</h1>
+      </Layout>
     </div>
   );
 };
